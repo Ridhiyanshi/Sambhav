d01e34473c78a767d383070eb60cb9ad0302e58c
Ridhiyanshi <81734649+Ridhiyanshi@users.noreply.github.com>
2023-10-17 20:17:21 +0530

    Add files via upload

---
+++ PRJ/poverty.js
@@ -0,0 +1,16 @@
+// function donate(){
+//     document.getElementById("a").animate({right:'350px'})
+// }
+// setInterval("donate()",1000)
+// $(document).ready(function(){
+//     $("#row1").hover(function(){
+//         $("#im1").animate({right:'500px'})
+//     })
+    
+// })
+// function a(){
+//     $(document).ready(function(){
+//         $('#h21').animate({right:'500px'})
+//     })
+// }    
+// setInterval('a()',3000)
